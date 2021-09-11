5756d08fea9b801c3e093c7346d00079afb83b68
MarcusStilwell <marcusstilwell@icloud.com>
2021-09-10 18:23:51 -0700

    Initial Commit

---
+++ Multiplication Flash Cards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Multiplication Flash Cards
+//
+//  Created by Marcus Stilwell on 9/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
