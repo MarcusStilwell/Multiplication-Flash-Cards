edcbf8acd1bf135e28b7497f0ba9a1773d3be847
MarcusStilwell <marcusstilwell@icloud.com>
2021-09-13 15:49:26 -0700

    Added stepper to allow for choosing which multiplication table being practiced

--- Multiplication Flash Cards/ContentView.swift
+++ Multiplication Flash Cards/ContentView.swift
@@ -10,9 +10,15 @@ import SwiftUI
 struct ContentView: View {
     @State private var inMenu = true
     @State private var inGame = true
+    @State private var multTable = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Stepper("Which multiplication table would you like to use? \n\n \(multTable)", value: $multTable, in: 0...12){_ in
+                
+            }
+        }
+            
     }
 }
 
