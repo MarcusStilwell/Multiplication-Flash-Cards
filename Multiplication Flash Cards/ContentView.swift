10d94078c7664535d1678d516caba2e3d2b4d896
MarcusStilwell <marcusstilwell@icloud.com>
2021-09-18 19:26:03 -0700

    Changed the way answers are received.

--- Multiplication Flash Cards/ContentView.swift
+++ Multiplication Flash Cards/ContentView.swift
@@ -11,12 +11,20 @@ struct ContentView: View {
     @State private var inMenu = true
     @State private var inGame = true
     @State private var multTable = 0
+    @State private var isAnswerCorrect = false
+    @State private var numberOfQuestions = ["5", "10", "15", "20", "All"]
+    @State private var numberOfQuestionsPlace = "5"
     
     var body: some View {
         VStack{
-            Stepper("Which multiplication table would you like to use? \n\n \(multTable)", value: $multTable, in: 0...12){_ in
-                
+            Stepper("Which multiplication table would you like to use? \n\n \(multTable)", value: $multTable, in: 0...12){
+                _ in
             }
+            Picker("Select the number of Questions", selection: $numberOfQuestionsPlace) {
+                            ForEach(numberOfQuestions, id: \.self) {
+                                Text($0)
+                            }
+                        }
         }
             
     }
